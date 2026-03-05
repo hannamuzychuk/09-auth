95148346242494df02b6118ec2d0e52654afa231
Hanna Muzychuk <hannamuzychuk@gmail.com>
2026-03-05 17:07:41 +0100

    add changes in route

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { api } from '../../api';
 import { cookies } from 'next/headers';
+import { logErrorResponse } from '../../_utils/utils';
 import { isAxiosError } from 'axios';
 
 type Props = {
@@ -18,10 +19,14 @@ export async function GET(request: Request, { params }: Props) {
     });
     return NextResponse.json(res.data, { status: res.status });
   } catch (error) {
-    if (isAxiosError(error) && error.response) {
-      console.error('API Error:', error.response.data);
-      return NextResponse.json(error.response.data, { status: error.response.status });
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
     }
+    logErrorResponse({ message: (error as Error).message });
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
@@ -38,10 +43,14 @@ export async function DELETE(request: Request, { params }: Props) {
     });
     return NextResponse.json(res.data, { status: res.status });
   } catch (error) {
-    if (isAxiosError(error) && error.response) {
-      console.error('API Error:', error.response.data);
-      return NextResponse.json(error.response.data, { status: error.response.status });
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
     }
+    logErrorResponse({ message: (error as Error).message });
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
@@ -59,10 +68,14 @@ export async function PATCH(request: Request, { params }: Props) {
     });
     return NextResponse.json(res.data, { status: res.status });
   } catch (error) {
-    if (isAxiosError(error) && error.response) {
-      console.error('API Error:', error.response.data);
-      return NextResponse.json(error.response.data, { status: error.response.status });
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
     }
+    logErrorResponse({ message: (error as Error).message });
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
